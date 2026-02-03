96b6a5ff44be41040b2927adfd87e742e26b5449
sultanjke <s.mecheyev@outlook.com>
2026-02-03 11:13:39 +0500

    Added Complete Practice 02 - For Loops

---
+++ Practice 02/for-loops/forLoopRange.py
@@ -0,0 +1,14 @@
+for x in range(2, 30, 3): # range(x, y, z) where x is starting point, y is ending point (not inclusive), z is step
+  print(x)
+
+# Output:
+# 2
+# 5
+# 8
+# 11
+# 14
+# 17
+# 20
+# 23
+# 26
+# 29
---
+++ Practice 02/for-loops/nestedLoops.py
@@ -0,0 +1,17 @@
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+# Output:
+# red apple
+# red banana
+# red cherry
+# big apple
+# big banana
+# big cherry
+# tasty apple
+# tasty banana
+# tasty cherry
